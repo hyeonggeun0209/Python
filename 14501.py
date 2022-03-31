4db30c46aecce2927326a01cc59167b27dd4aa1f
hyeonggeun0209 <enddj99@naver.com>
2022-03-31 19:34:48 +0900

    Baekjoon 14501

---
+++ 14501.py
@@ -0,0 +1,23 @@
+from sys import stdin
+def com(list,i,N,d):
+    if i+list[i][0] > N or d[i] != 0:
+        return
+    elif i+list[i][0] == N:
+        d[i] += list[i][1]
+        return
+    else:
+        for j in range(i+list[i][0],N):
+            if j+list[j][0] <= N:
+                if(d[j] == 0) : com(list,j,N,d)
+                d[i] = max(d[i],d[j])
+        d[i] += list[i][1]
+
+N = int(stdin.readline())
+con = []
+d = [0] * N
+for i in range(N):
+    con.append(list(map(int, stdin.readline().split())))
+#print(con)
+for i in range(N):
+    com(con,i,N,d)
+print(max(d))
